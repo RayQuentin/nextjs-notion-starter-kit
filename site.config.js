f1a43c34eea040e64804020c924412ba436276c2
RayQuentin <r.p.quentin@gmail.com>
2021-09-19 16:13:24 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: '',
   github: 'RayQuentin',
-  linkedin: '秦锐',
+  linkedin: 'Rui Qin',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
